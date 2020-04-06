82570cb2ccf1e1d4466c537826c6721f80b2cd7d
Tucker Chapman <tucker.r.chapman@gmail.com>
2020-04-05 20:45:29 -0400

    Calculate random color for each county

--- countyToJson.py
+++ countyToJson.py
@@ -45,11 +45,11 @@ def main():
             '''
             if state in list(state_data.keys()):
                 if county in list(state_data[state].keys()):
-                    i = len(state_data[state][county])
-                    prev = state_data[state][county][i-1]
+                    i = len(state_data[state][county]['data'])
+                    prev = state_data[state][county]['data'][i-1]
                     c_delta = cases - prev[header[4]]
                     d_delta = deaths - prev[header[5]]
-                    state_data[state][county].append({
+                    state_data[state][county]['data'].append({
                         header[0]: date,
                         header[4]: cases,
                         header[5]: deaths,
@@ -57,8 +57,12 @@ def main():
                         "d_delta": d_delta,
                     })
                 else:
-                    state_data[state][county] = []
-                    state_data[state][county].append({
+                    count = len(list(state_data[state].keys())) + 1
+                    state_data[state][county] = {
+                        'data': [],
+                        'color': 'hsla({}, 100%, 50%, 1)'.format(count * 6)
+                    }
+                    state_data[state][county]['data'].append({
                         header[0]: date,
                         header[4]: cases,
                         header[5]: deaths,
@@ -67,13 +71,16 @@ def main():
                     })
             else:
                 state_data[state] = {
-                    county: [{
-                        header[0]: date,
-                        header[4]: cases,
-                        header[5]: deaths,
-                        "c_delta": cases,
-                        "d_delta": deaths
-                    }]
+                    county: {
+                        'data': [{
+                            header[0]: date,
+                            header[4]: cases,
+                            header[5]: deaths,
+                            "c_delta": cases,
+                            "d_delta": deaths
+                        }],
+                        'color': 'hsla( {}, 100%, 50%, 1)'.format(1 * 6)
+                    }
                 }
     with open('us-counties.json', 'w') as output:
         output.write(json.dumps(state_data))
